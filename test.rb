eec70c8925f150f603c7d504e0a94500793effe8
Porfirio Matias <SSJ6Porfy@hotmail.com>
2018-01-04 16:59:48 -0800

    christmas algorithms

--- test.rb
+++ test.rb
@@ -141,13 +141,177 @@ def prime?(n)
     true
 end
 
-arr.each do |num|
-    if num == 1
-        puts 'Not prime'
-    elsif prime?(num)
-        puts 'Prime'
-    else
-        puts 'Not prime'
+# arr.each do |num|
+#     if num == 1
+#         puts 'Not prime'
+#     elsif prime?(num)
+#         puts 'Prime'
+#     else
+#         puts 'Not prime'
+#     end
+# end
+
+def sudoku2(grid)
+    
+    i = 0
+    
+    while i < grid.length
+        hash = Hash.new(0)
+        j = 0
+
+        while j < grid[0].length
+            hash[grid[i][j]] += 1
+            return false if hash[grid[i][j]] > 1 && grid[i][j] != '.'
+            j += 1
+        end
+        i += 1
     end
+
+    i = 0
+
+    while i < grid[0].length
+        hash = Hash.new(0)
+        j = 0
+
+        while j < grid.length
+            hash[grid[j][i]] += 1
+            return false if hash[grid[j][i]] > 1 && grid[j][i] != '.'
+            j += 1
+        end
+        i += 1
+    end
+
+    true && grid_builder(grid)
 end
 
+def grid_builder(grid)
+    grid1 = Hash.new(0)
+    grid2 = Hash.new(0)
+    grid3 = Hash.new(0)
+    grid4 = Hash.new(0)
+    grid5 = Hash.new(0)
+    grid6 = Hash.new(0)
+    grid7 = Hash.new(0)
+    grid8 = Hash.new(0)
+    grid9 = Hash.new(0)
+    
+    i = 0
+
+    while i < grid.length
+
+        j = 0
+
+        while j < grid[0].length
+            grid1[grid[i][j]] += 1 if i.between?(0,2) && j.between?(0,2)
+            grid2[grid[i][j]] += 1 if i.between?(0,2) && j.between?(3,5)
+            grid3[grid[i][j]] += 1 if i.between?(0,2) && j.between?(6,8)
+            grid4[grid[i][j]] += 1 if i.between?(3,5) && j.between?(0,2)
+            grid5[grid[i][j]] += 1 if i.between?(3,5) && j.between?(3,5)
+            grid6[grid[i][j]] += 1 if i.between?(3,5) && j.between?(6,8)
+            grid7[grid[i][j]] += 1 if i.between?(6,8) && j.between?(0,2)
+            grid8[grid[i][j]] += 1 if i.between?(6,8) && j.between?(3,5)
+            grid9[grid[i][j]] += 1 if i.between?(6,8) && j.between?(6,8)
+            
+            return false if grid1[grid[i][j]] > 1 && grid[j][i] != '.' 
+            return false if grid2[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid3[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid4[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid5[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid6[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid7[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid8[grid[i][j]] > 1 && grid[j][i] != '.'
+            return false if grid9[grid[i][j]] > 1 && grid[j][i] != '.'
+            
+            j += 1
+        end
+        i += 1
+    end
+    
+    true
+end
+
+    grid = [[".",".",".","1","4",".",".","2","."], 
+    [".",".","6",".",".",".",".",".","."], 
+    [".",".",".",".",".",".",".",".","."], 
+    [".",".","1",".",".",".",".",".","."], 
+    [".","6","7",".",".",".",".",".","9"], 
+    [".",".",".",".",".",".","8","1","."], 
+    [".","3",".",".",".",".",".",".","6"], 
+    [".",".",".",".",".","7",".",".","."], 
+    [".",".",".","5",".",".",".","7","."]]
+
+    # sudoku2(grid)
+
+
+    crypt = ["SEND", 
+    "MORE", 
+    "MONEY"]
+   solution = [["O","0"], 
+    ["M","1"], 
+    ["Y","2"], 
+    ["E","5"], 
+    ["N","6"], 
+    ["D","7"], 
+    ["R","8"], 
+    ["S","9"]]
+
+    def isCryptSolution(crypt, solution)
+        
+            hash = {}
+            
+            solution.each { |key_pair| hash[key_pair[0]] = key_pair[1] }
+            
+            result = crypt.map { |str| str.chars.map{ |chr| hash[chr] }.join('') }
+            
+            result.each { |num| return false if num[0] == '0' }
+            
+            result = result.map(&:to_i)
+            
+            result[0] + result [1] == result[2]
+    end
+
+    # p isCryptSolution(crypt, solution)
+
+stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
+
+def max_profit(arr)
+    profit = nil
+
+    0.upto(arr.length - 2).each do |i|
+        (i + 1).upto(arr.length - 1).each do |j|
+            curr = arr[j] - arr[i]
+            profit = curr if profit.nil? || curr > profit
+        end    
+    end
+    
+    profit
+end
+
+# p max_profit(stock_prices_yesterday)
+def almostIncreasingSequence(sequence)
+    
+    idx = 0
+    while idx < sequence.length
+        arr = []
+        j = 0
+        while j < sequence.length
+            if j == idx
+                j += 1
+                next
+            end
+            break if arr.last && arr.last >= sequence[j]
+            arr << sequence[j]
+            return true if arr.length == sequence.length - 1
+            j += 1
+        end
+        idx += 1
+    end
+    
+    false
+end
+
+
+
+arr = [3, 5, 67, 98, 3]
+
+p almostIncreasingSequence(arr)
